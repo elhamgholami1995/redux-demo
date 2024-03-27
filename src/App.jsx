f7754462e47463c9cea9bd2ef5037c7fab58f77a
elhamgholami1995 <elhamgholami5531@gmail.com>
2024-03-27 22:39:59 +0330

    add provider in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,15 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Provider } from "react-redux";
+import store from "./Redux/cake/store";
 
 function App() {
-  return <h1>REDUX</h1>;
+  return (
+    <Provider store={store}>
+      <h1>REDUX</h1>
+    </Provider>
+  );
 }
 
 export default App;
