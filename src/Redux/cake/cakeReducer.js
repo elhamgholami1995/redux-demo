3b071305a42bd9fd02772abb5525cd915f77378a
elhamgholami1995 <elhamgholami5531@gmail.com>
2024-03-27 22:35:53 +0330

    create cakereducer.js file and write cakereducer function

---
+++ src/Redux/cake/cakeReducer.js
@@ -0,0 +1,18 @@
+const initialState = {
+  numOfCakes: 10,
+};
+
+function cakeReducer(state = numOfCakes, action) {
+  switch (action.type) {
+    case "BUY_CAKE": {
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+    }
+    default: 
+      return state;
+  }
+}
+
+export default cakeReducer;
