f68b75e793b87e6c91c113e7b3214be4beca894c
elhamgholami1995 <elhamgholami5531@gmail.com>
2024-03-28 05:46:56 +0330

    use useSelector and useDispatch in cakecontainer component

--- src/Redux/cake/cakeReducer.js
+++ src/Redux/cake/cakeReducer.js
@@ -2,12 +2,12 @@ const initialState = {
   numOfCakes: 10,
 };
 
-function cakeReducer(state= "numOfCakes", action) {
+function cakeReducer(state = initialState, action) {
   switch (action.type) {
     case "BUY_CAKE": {
       return {
         ...state,
-        numOfCakes: state.numOfCakes - 1,
+        numOfCakes: state.numOfCakes - action.payload,
       };
     }
     default:
--- src/components/CakeContainer.jsx
+++ src/components/CakeContainer.jsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
 function CakeContainer() {
+  const state = useSelector((state) => state);
+  const dispatch = useDispatch();
+  const [value, setValue] = useState("");
   return (
     <div>
-      <h2>Cake Number:10</h2>
-      <button>Buy Cake</button>
+      <h2>Cake Number:{state.numOfCakes}</h2>
+      <input
+        type="number"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button onClick={() => dispatch({ type: "BUY_CAKE", payload: value })}>
+        Buy Cake
+      </button>
     </div>
   );
 }
