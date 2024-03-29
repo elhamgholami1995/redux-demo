d10a37f8140a8200fa1cd31414a897db1e2a97d4
elhamgholami1995 <elhamgholami5531@gmail.com>
2024-03-30 00:18:51 +0330

    install @redux-devtool

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -2,7 +2,10 @@ import { applyMiddleware, createStore } from "redux";
 import cakeReducer from "./cake/cakeReducer";
 import rootReducer from "./rootReducer";
 import { thunk } from "redux-thunk";
+import { composeWithDevTools } from "@redux-devtools/extension";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 export default store;
- 
